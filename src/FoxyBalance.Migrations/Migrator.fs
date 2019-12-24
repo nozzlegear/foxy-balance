a7d4a4ad408b1349d89df954990473b774c7ba73
Joshua Harms <joshua@nozzlegear.com>
2019-12-24 14:07:35 -0600

    Add function for using migrations to migrate database up or down

--- src/FoxyBalance.Migrations/Migrator.fs
+++ src/FoxyBalance.Migrations/Migrator.fs
@@ -1,4 +1,31 @@
 ﻿namespace FoxyBalance.Migrations
 
+open System.Data.Common
+open SimpleMigrations
+open SimpleMigrations.DatabaseProvider
+open System.Data.SqlClient
+
 module Migrator =
-    ()
+    type MigrationTarget =
+        | Latest
+        | Baseline of int64
+        | Target of int64
+    
+    /// Migrates the SQL database to the desired target migration.
+    let migrate direction (connStr : string) =
+        let assembly = typeof<FoxyBalance.Migrations.Migration_01>.Assembly
+        
+        use connection = new SqlConnection(connStr)
+        connection.Open()
+        let provider = MssqlDatabaseProvider connection
+        let migrator = SimpleMigrator(assembly, provider)
+        
+        migrator.Load()
+        
+        match direction with
+        | Latest ->
+            migrator.MigrateToLatest()
+        | Baseline target ->
+            migrator.Baseline target
+        | Target target ->
+            migrator.MigrateTo target
