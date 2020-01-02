3843506c06bc0b1068f788bb551a6e4d53476635
Joshua Harms <joshua@nozzlegear.com>
2020-01-01 21:34:31 -0600

    Add Task module for common task utility functions

---
+++ src/FoxyBalance.Server/Task.fs
@@ -0,0 +1,24 @@
+﻿[<AutoOpen>]
+module FoxyBalance.Server.Task
+    open FSharp.Control.Tasks.V2.ContextInsensitive
+
+    /// Maps the result of a task.
+    let inline Map fn (computation : System.Threading.Tasks.Task<_>) =
+        task {
+            let! result = computation
+            return fn result 
+        }
+        
+    /// Maps the result of one task to another
+    let inline Bind (fn : _ -> System.Threading.Tasks.Task<_>) (computation : System.Threading.Tasks.Task<_>) =
+        task {
+            let! result = computation
+            return! fn result 
+        }
+    
+    /// Ignores the result of a task. 
+    let inline Ignore (computation : System.Threading.Tasks.Task<_>) =
+        task {
+            let! result = computation
+            ignore result 
+        } :> System.Threading.Tasks.Task 
