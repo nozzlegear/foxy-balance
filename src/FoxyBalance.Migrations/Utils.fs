864661dd4aefc3d99875e484d47eda2be97b702c
Joshua Harms <joshua@nozzlegear.com>
2022-09-18 15:38:48 -0500

    Add utility functions for reading sql migrations from sql files

---
+++ src/FoxyBalance.Migrations/Utils.fs
@@ -0,0 +1,28 @@
+namespace FoxyBalance.Migrations
+
+open System
+open System.IO
+open System.Reflection
+open System.Text
+open System.Text.RegularExpressions
+
+module Utils = 
+    let private getPath fileName = 
+        // Use the assembly to get the current directory, as it can switch between bin and project directory
+        // https://github.com/dotnet/project-system/issues/589
+        let currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)
+
+        Path.Combine(currentDirectory, "Migrations", "sql", fileName)
+
+    let readSqlFile fileName : string = 
+        getPath fileName
+        |> File.ReadAllText
+
+    let readSqlFileBatches fileName : string seq = 
+        let text = readSqlFile fileName
+        let goRegex = Regex("^GO[;| ]*$", RegexOptions.Multiline)
+
+        // Split the text of the file by each GO, removing empty entries after splitting (e.g. if the last line in the file is "GO")
+        goRegex.Split text
+        |> Seq.choose (fun str -> if String.IsNullOrWhiteSpace str then None else Some str)
+
