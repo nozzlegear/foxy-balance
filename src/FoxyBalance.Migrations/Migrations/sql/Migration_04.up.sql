a19647cf866297793c217b245a188e38f0aee540
Joshua Harms <joshua@nozzlegear.com>
2022-09-18 16:41:18 -0500

    Add [SaleDate] column

--- src/FoxyBalance.Migrations/Migrations/sql/Migration_04.up.sql
+++ src/FoxyBalance.Migrations/Migrations/sql/Migration_04.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE [FoxyBalance_IncomeRecords] (
     [Id] BIGINT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
     [UserId] INT NOT NULL CONSTRAINT [FK_IncomeRecords_UserId] FOREIGN KEY REFERENCES FoxyBalance_Users(Id),
     [TaxYearId] INT NOT NULL CONSTRAINT [FK_IncomeRecords_TaxYearId] FOREIGN KEY REFERENCES FoxyBalance_TaxYears(Id),
+    [SaleDate] DATETIMEOFFSET NOT NULL,
     [SourceType] NVARCHAR(18) NOT NULL,
     [SourceTransactionId] NVARCHAR(255) NULL,
     [ManualTransactionDescription] NVARCHAR(1000) NULL,
