3f27af3d0c9cbaa7aaa498eda65c3f0834128505
Joshua Harms <joshua@nozzlegear.com>
2022-10-13 20:04:02 -0500

    Remove unique constraint on SourceTransactionId
    SQL Server does not allow unique columns to contain multiple null values, which is not compatible with these income records. Shopify refund transactions do not have a transaction id and therefore would count as duplicate values since they're null.

--- src/FoxyBalance.Migrations/Migrations/sql/Migration_04.down.sql
+++ src/FoxyBalance.Migrations/Migrations/sql/Migration_04.down.sql
@@ -1,5 +1,9 @@
 ALTER TABLE [FoxyBalance_IncomeRecords]
-DROP CONSTRAINT [FK_IncomeRecords_TaxYearId], [FK_IncomeRecords_UserId], [UN_IncomeRecords_SourceTransactionId]
+DROP CONSTRAINT [FK_IncomeRecords_TaxYearId], [FK_IncomeRecords_UserId]
+
+GO;
+
+DROP INDEX [IDX_IncomeRecords_SourceTransactionId] ON [FoxyBalance_IncomeRecords]
 
 GO;
 
