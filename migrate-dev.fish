cd005a13103e1557383c69ed6eb25e3ad4103782
Joshua Harms <joshua@nozzlegear.com>
2024-04-02 16:26:31 -0500

    chore: update dev database port

--- migrate-dev.fish
+++ migrate-dev.fish
@@ -9,6 +9,6 @@ if not count $argv > /dev/null
 end
 
 # Assume target database is the container used in dev
-set connectionString "Server=localhost,7021;Database=master;User Id=sa;Password=a-BAD_passw0rd"
+set connectionString "Server=localhost,1433;Database=master;User Id=sa;Password=a-BAD_passw0rd"
 
 dotnet run --project ./src/FoxyBalance.Migrator -- $argv -c "$connectionString"
