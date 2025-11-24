630e68005f39eda7f9e7aef12f264416df939fe1
Joshua Harms <joshua@nozzlegear.com>
2025-11-23 22:54:41 -0600

    Add Migrator `down` verb
    FluentMigrations doesn't (easily) support a `to X` verb where `X` is a
    migration that's below the current migration, i.e. it does not easily
    support going down in a targeted manner because it doesn't know which
    migration the database is on before it runs. We work around this
    limitation by letting the CLI specify which direction it should go
    explicitly.
    
    type: feature
    scope: migrations, database

--- src/FoxyBalance.Migrator/Options.fs
+++ src/FoxyBalance.Migrator/Options.fs
@@ -5,11 +5,11 @@ open CommandLine
 type ExitType =
     | Success
     | Failure
-    
+
 type RunResult =
     | Exit of ExitType
     | ShowHelp of ExitType
-    | Problem of string 
+    | Problem of string
 
 [<Verb("up", HelpText = "Migrates the database up to the newest version.")>]
 type UpOptions = {
@@ -17,20 +17,29 @@ type UpOptions = {
     connectionString : string
 }
 
-[<Verb("to", HelpText = "Migrates the database up or down to a specific version.")>]
+[<Verb("to", HelpText = "Migrates the database up to a specific version.")>]
 type ToOptions = {
     [<Option('c', "connection-string", Required = true, HelpText = "Connection string for the target database")>]
     connectionString : string
-   
+
     [<Value(0, Required = true, MetaName = "version", HelpText = "Target version")>]
-    value : int64 
+    value : int64
+}
+
+[<Verb("down", HelpText = "Migrates the database down to a specific version.")>]
+type DownOptions = {
+    [<Option('c', "connection-string", Required = true, HelpText = "Connection string for the target database")>]
+    connectionString : string
+
+    [<Value(0, Required = true, MetaName = "version", HelpText = "Target version")>]
+    value : int64
 }
 
 [<Verb("baseline", HelpText = "Baselines the database to a specific version. This marks the database as having migrated to that version, but does not actually run the migrations to get there.")>]
 type BaselineOptions = {
     [<Option('c', "connection-string", Required = true, HelpText = "Connection string for the target database")>]
     connectionString : string
-   
+
     [<Value(0, Required = true, MetaName = "version", HelpText = "Target version")>]
-    value : int64 
+    value : int64
 }
