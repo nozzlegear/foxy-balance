f670849fe6e4106607b55084b20b073f10d18339
Joshua Harms <joshua@nozzlegear.com>
2025-11-27 23:10:19 -0600

    Prune unused `Utils.dictionaryToMap` and `IncomeRecord.Default` functions
    type: chore
    scope: utils

--- src/FoxyBalance.Database/Utils.fs
+++ src/FoxyBalance.Database/Utils.fs
@@ -3,14 +3,6 @@ namespace FoxyBalance.Database
 open System.Data
 open System.Threading.Tasks
 
-[<AutoOpen>]
-module internal Utils =
-    let inline dictionaryToMap d =
-        // Source: https://gist.github.com/theburningmonk/3363893
-        d :> seq<_>
-        |> Seq.map (|KeyValue|)
-        |> Map.ofSeq
-
 module internal Sql =
     /// Returns the only element in the result, or None if the result is empty or more than one element is returned.
     let tryExactlyOne (job : Task<'a list>) = task {
